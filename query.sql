a6490cf721347c67348d2242e9537d1389bb31e4
Mae-Hyong <maehyong02@gmail.com>
2024-04-01 19:40:11 +0900

    chore : first query

---
+++ query.sql
@@ -0,0 +1,14 @@
+create table user_join_info(
+	user_id varchar(50) primary key,
+    user_password varchar(250),
+    user_role ENUM('General Member', 'President', 'Manager'), -- 일반 회원, 사장님, 관리자
+    join_date datetime default now(),
+    password_update datetime default now() on update now() 
+);
+
+create table user_info(
+	user_id varchar(50) primary key,
+    user_nickname varchar(10) unique,
+    user_image longtext,
+    phone_number varchar(15)
+);
