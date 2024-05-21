4d2df682c207d7241d6b57005c8f3c1a7e0985f9
Mae-Hyong <maehyong02@gmail.com>
2024-05-21 15:54:41 +0900

    chore : redirect URL Modify

--- controllers/payController.js
+++ controllers/payController.js
@@ -65,7 +65,7 @@ const payController = {
             
             await payModel.payments(paymentsData)
 
-            res.send(response.data.next_redirect_pc_url);
+            res.send(response.data.next_redirect_app_url);
         } catch (error) {
             console.error("카카오페이 결제 요청 실패:", error.message);
             res.status(500).send("카카오페이 결제 요청 실패");
