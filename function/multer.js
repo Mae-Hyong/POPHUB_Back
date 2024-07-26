49aa8eb87dbbd29979096dfd39843a64ced25d64
Hyeo_Ni__ <maehyong02@gmail.com>
2024-07-26 23:56:40 +0900

    fix : Multer Modify

--- function/multer.js
+++ function/multer.js
@@ -1,35 +1,26 @@
-const multer = require("multer");
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const { S3Client } = require('@aws-sdk/client-s3');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
 const { v4 } = require("uuid");
 require('dotenv').config();
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_KEY, 
-    api_secret: process.env.CLOUDINARY_SECRET 
-});
-
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-const day = currentDate.getDate().toString().padStart(2, '0');
-const hours = currentDate.getHours().toString().padStart(2, '0');
-const minutes = currentDate.getMinutes().toString().padStart(2, '0');
-const seconds = currentDate.getSeconds().toString().padStart(2, '0');
-const timestamp = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params:{
-      folder:'user_images',
-      allowedFormats: ['jpeg', 'png', 'jpg'],
-      public_id: (req, file) => {
-        return timestamp + "_" + v4()
-      },
+const s3 = new S3Client({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.accessKey,
+    secretAccessKey: process.env.secretKey,
   },
 });
 
-const upload = multer({ storage: storage });
+// Multer와 multerS3 설정
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'pophub2024',
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString()); // 업로드 시 파일명 변경
+    },
+  }),
+});
 
 module.exports = upload;
