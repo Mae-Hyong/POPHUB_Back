ecdc65bc4a60db57ad4fbaa5957404dcd3ff3402
Lee Jinsol <n.solee9@gmail.com>
2024-08-30 16:00:40 +0900

    feat: add showUserDelivery

--- controllers/deliveryController.js
+++ controllers/deliveryController.js
@@ -35,7 +35,7 @@ const deliveryController = {
         try {
             const { addressId, address } = req.body;
             await deliveryModel.updateAddress(address, addressId);
-            res.status(200).json({ message: "주소가 수정되었습니다."});
+            res.status(200).json({ message: "주소가 수정되었습니다." });
         } catch (err) {
             res.status(500).send("주소 수정 중 오류가 발생하였습니다.");
         }
@@ -82,6 +82,30 @@ const deliveryController = {
             res.status(200).json({ message: "주문이 취소되었습니다." });
         } catch (err) {
             res.status(500).send("주문 취소 중 오류가 발생하였습니다.");
+        }
+    },
+
+    // 배송 주문 조회
+    showUserDelivery: async (req, res) => {
+        try {
+            const { userName, status } = req.query;
+            let result;
+            const getStatusMapping = {
+                'All': '전체',
+                'Order Completed': '주문 완료',
+                'Order Canceled': '주문 취소',
+                'Shipping': '배송중',
+                'Delivered': '배송 완료'
+            };
+            const getStatus = getStatusMapping[status];
+            result = await deliveryModel.statusDelivery(userName, getStatus);
+            if (!result || (Array.isArray(result) && result.length === 0)) {
+                result = { message: "해당 내역이 존재하지 않습니다." };
+            }
+    
+            res.status(200).json(result);
+        } catch (err) {
+            res.status(500).send("주문 조회 중 오류가 발생하였습니다.");
         }
     },
 }
