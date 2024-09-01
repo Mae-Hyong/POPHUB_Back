c488dc42b7232dc00ae4405068107465e330f106
Hyeo_Ni__ <maehyong02@gmail.com>
2024-09-01 23:33:45 +0900

    fix: funding Modify

--- controllers/fundingController.js
+++ controllers/fundingController.js
@@ -1,7 +1,162 @@
 const fundingModel = require('../models/fundingModel');
 
 const fundingController = {
+    createFunding: async(req, res) => {
+        try {
+            const body = req.body;
+            let image = req.file ? req.file.location : null;
+            const fundingData = {
+                user_name: body.userName,
+                title: body.title,
+                content: body.content,
+                amount: body.amount,
+                donation: body.donation,
+                open_date: body.openDate,
+                close_date: body.closeDate
+            };
 
+            await fundingModel.createFunding(fundingData);
+
+            if(image){
+                const fundingImg = {
+                    funding_id: fundingId,
+                    image: image,
+                }
+
+                await fundingModel.fundingImg(fundingImg);
+            }
+            
+            return res.status(201).send('Funding Data Added');
+        } catch (err) {
+            return res.status(500).send('Funding 데이터를 입력 도중 오류가 발생했습니다.');
+        }
+    },
+
+    createItem: async(req, res) => {
+        try {
+            const body = req.body;
+            let image = req.file ? req.file.location : null;
+            const itemData = {
+                funding_id: body.fundingId,
+                user_name: body.userName,
+                item_name: body.title,
+                content: body.content,
+                count: body.count,
+                amount: body.amount,
+            };
+
+            await fundingModel.createItem(itemData);
+
+            if(image){
+                const fundingImg = {
+                    funding_id: fundingId,
+                    image: image,
+                }
+
+                await fundingModel.itemImg(fundingImg);
+            }
+            
+            return res.status(201).send('Item Data Added');
+        } catch (err) {
+            return res.status(500).send('Item 데이터를 입력 도중 오류가 발생했습니다.');
+        }
+    },
+
+    // createlist: async(req, res) => {
+    //     try {
+    //         const body = req.body;
+    //         let image = req.file ? req.file.location : null;
+    //         const listData = {
+    //             funding_id: body.fundingId,
+    //             item_id: body.itemId,
+    //             partner_order_id: body.partnerOrderId,
+    //             user_name: body.userName,
+    //         };
+
+    //         await fundingModel.createList(listData);
+            
+    //         return res.status(201).send('Item Data Added')
+    //     } catch (err) {
+            
+    //     }
+    // },
+
+    searchFunding: async(req, res) => {
+        try {
+            const fundingId = req.query;
+            const userName = req.query;
+            if (!fundingId && !userName){
+                const result = await fundingModel.searchFunding();
+                const fundingList = await Promise.all(
+                    result.map(async (result) => {
+                        const images = await fundingModel.imagesByFundingId(fundingId);
+                        return {
+                            fundingId: result.funding_id,
+                            userName: result.userName,
+                            title: result.title,
+                            content: result.content,
+                            amount: result.amount,
+                            donation: result.donation,
+                            status: result.status,
+                            openDate: result.openDate,
+                            closeDate: result.closeDate,
+                            images: images.map(image => image.image_url)
+                        };
+                    })
+                );
+                return res.status(200).send(fundingList);
+            } else if(fundingId) {
+                const funding = await fundingModel.fundingById(fundingId);
+                const images = await fundingModel.imagesByFundingId(fundingId);
+                return res.status(200).json({funding, images: images.map(image => image.image_url)})
+            } else {
+                const funding = await fundingModel.fundingByUser(userName)
+                return res.status(200).send(funding)
+            }
+        } catch (err) {
+            res.status(500).send("펀딩 조회 중 오류 발생")
+        }
+    },
+
+    searchItem: async(req, res) => {
+        try {
+            
+        } catch (err) {
+            
+        }
+    },
+
+    searchlist: async(req, res) => {
+        try {
+            
+        } catch (err) {
+            
+        }
+    },
+
+    updateFunding: async(req, res) => {
+        try {
+            
+        } catch (err) {
+            
+        }
+    },
+
+    updateItem: async(req, res) => {
+        try {
+            
+        } catch (err) {
+            
+        }
+    },
+
+    updatelist: async(req, res) => {
+        try {
+            
+        } catch (err) {
+            
+        }
+    },
 }
 
 module.exports = fundingController;
--- router/fundingRouter.js
+++ router/fundingRouter.js
@@ -1,4 +1,135 @@
 const express = require('express');
+const fundingController = require('../controllers/fundingController');
 const router = express.Router();
+
+/**
+ * @swagger
+ * /funding/create:
+ *   post:
+ *     summary: 새로운 펀딩을 생성합니다.
+ *     tags: [Funding]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userName:
+ *                 type: string
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *               amount:
+ *                 type: number
+ *               donation:
+ *                 type: number
+ *               openDate:
+ *                 type: string
+ *                 format: date
+ *               closeDate:
+ *                 type: string
+ *                 format: date
+ *               image:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       201:
+ *         description: Funding Data Added
+ *       500:
+ *         description: Funding 데이터를 입력 도중 오류가 발생했습니다.
+ */
+
+/**
+ * @swagger
+ * /funding/item/create:
+ *   post:
+ *     summary: 펀딩 아이템을 생성합니다.
+ *     tags: [Funding]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fundingId:
+ *                 type: integer
+ *               userName:
+ *                 type: string
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *               count:
+ *                 type: integer
+ *               amount:
+ *                 type: number
+ *               image:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       201:
+ *         description: Item Data Added
+ *       500:
+ *         description: Item 데이터를 입력 도중 오류가 발생했습니다.
+ */
+
+/**
+ * @swagger
+ * /funding/search:
+ *   get:
+ *     summary: 펀딩을 검색합니다.
+ *     tags: [Funding]
+ *     parameters:
+ *       - in: query
+ *         name: fundingId
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: 검색할 펀딩의 ID
+ *       - in: query
+ *         name: userName
+ *         required: false
+ *         schema:
+ *           type: string
+ *         description: 검색할 펀딩의 사용자 이름
+ *     responses:
+ *       200:
+ *         description: 성공적으로 펀딩 데이터를 반환합니다.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 fundingId:
+ *                   type: integer
+ *                 userName:
+ *                   type: string
+ *                 title:
+ *                   type: string
+ *                 content:
+ *                   type: string
+ *                 amount:
+ *                   type: number
+ *                 donation:
+ *                   type: number
+ *                 status:
+ *                   type: string
+ *                 openDate:
+ *                   type: string
+ *                   format: date
+ *                 closeDate:
+ *                   type: string
+ *                   format: date
+ *                 images:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                     format: url
+ *       500:
+ *         description: 펀딩 조회 중 오류 발생
+ */
 
 module.exports = router;
