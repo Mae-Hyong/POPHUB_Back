1761ab99a79343fc0ef4dcda6415a4aaf62352eb
Promark1225 <wsw7854@naver.com>
2024-10-22 09:47:00 +0900

    fix: achieve alarm

--- models/achieveModel.js
+++ models/achieveModel.js
@@ -1,27 +1,67 @@
-const db = require('../config/mysqlDatabase');
+const db = require("../config/mysqlDatabase");
+const alarmService = require("../function/alarmService");
 
-const insert_achieveHub_query = "INSERT IGNORE INTO achieve_hub (user_name, achieve_id) VALUES (?, ?)";
-const insert_point_query = "INSERT INTO point_history SET ?"
+const insert_achieveHub_query =
+    "INSERT IGNORE INTO achieve_hub (user_name, achieve_id) VALUES (?, ?)";
+const insert_point_query = "INSERT INTO point_history SET ?";
 
-const search_achieveHub_query = 'SELECT * FROM achieve_hub WHERE user_name = ?';
-const select_achieveHub_query = "SELECT * FROM achieve_hub WHERE user_name = ? AND achieve_id = ?"
-const select_achieve_query = "SELECT * FROM achieve WHERE achieve_id = ?"
-const count_mark_query = 'SELECT COUNT(*) AS markCount FROM BookMark WHERE user_name = ?';
+const search_achieveHub_query = "SELECT * FROM achieve_hub WHERE user_name = ?";
+const select_achieveHub_query =
+    "SELECT * FROM achieve_hub WHERE user_name = ? AND achieve_id = ?";
+const select_achieve_query = "SELECT * FROM achieve WHERE achieve_id = ?";
+const count_mark_query =
+    "SELECT COUNT(*) AS markCount FROM BookMark WHERE user_name = ?";
 const joinDate_count_query = `
     SELECT uj.user_id, ui.user_name 
     FROM user_join_info uj 
     INNER JOIN user_info ui ON uj.user_id = ui.user_id
     WHERE uj.user_id = ? AND DATEDIFF(NOW(), uj.join_date) >= 10`;
 
-
 const achieveModel = {
     clearAchieve: async (userName, achieveId) => {
         return new Promise((resolve, reject) => {
-            db.query(insert_achieveHub_query, [userName, achieveId], (err, result) => {
-                if (err) reject(err);
-                else resolve(result);
-            })
-        })
+            db.query(
+                insert_achieveHub_query,
+                [userName, achieveId],
+                (err, result) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    db.query(
+                        select_achieve_query,
+                        [achieveId],
+                        (err, achieveResult) => {
+                            if (err) {
+                                return reject(err);
+                            }
+
+                            if (achieveResult.length > 0) {
+                                const achieveName = achieveResult[0].name;
+                                const message = `${userName}님 ${achieveName} 업적을 달성했습니다!`;
+
+                                alarmService
+                                    .sendNotification(userName, message)
+                                    .then(() => {
+                                        resolve(result);
+                                    })
+                                    .catch((alarmErr) => {
+                                        console.error(
+                                            "알림 전송 중 오류 발생:",
+                                            alarmErr
+                                        );
+                                        resolve(result);
+                                    });
+                            } else {
+                                return reject(
+                                    new Error("업적 정보를 찾을 수 없습니다.")
+                                );
+                            }
+                        }
+                    );
+                }
+            );
+        });
     },
 
     selectAchive: async (achieveId) => {
@@ -29,8 +69,8 @@ const achieveModel = {
             db.query(select_achieve_query, achieveId, (err, result) => {
                 if (err) reject(err);
                 else resolve(result[0]);
-            })
-        })
+            });
+        });
     },
 
     searchAchiveHub: async (userName) => {
@@ -38,17 +78,21 @@ const achieveModel = {
             db.query(search_achieveHub_query, userName, (err, result) => {
                 if (err) reject(err);
                 else resolve(result);
-            })
-        })
+            });
+        });
     },
 
     selectAchiveHub: async (userName, achieveId) => {
         return new Promise((resolve, reject) => {
-            db.query(select_achieveHub_query, [userName, achieveId], (err, result) => {
-                if (err) reject(err);
-                else resolve(result[0]);
-            })
-        })
+            db.query(
+                select_achieveHub_query,
+                [userName, achieveId],
+                (err, result) => {
+                    if (err) reject(err);
+                    else resolve(result[0]);
+                }
+            );
+        });
     },
 
     countBookMark: async (userName) => {
@@ -56,8 +100,8 @@ const achieveModel = {
             db.query(count_mark_query, userName, (err, result) => {
                 if (err) reject(err);
                 else resolve(result);
-            })
-        })
+            });
+        });
     },
 
     checkAndAddAchieve: (userId) => {
@@ -66,10 +110,14 @@ const achieveModel = {
                 if (err) return reject(err);
                 if (result.length > 0) {
                     const { userName } = result[0];
-                    db.query(insert_achieveHub_query, [userName, 9], (err, result) => {
-                        if (err) reject(err);
-                        else resolve(result);
-                    });
+                    db.query(
+                        insert_achieveHub_query,
+                        [userName, 9],
+                        (err, result) => {
+                            if (err) reject(err);
+                            else resolve(result);
+                        }
+                    );
                 } else {
                     resolve(null);
                 }
@@ -82,9 +130,9 @@ const achieveModel = {
             db.query(insert_point_query, insertData, (err, result) => {
                 if (err) reject(err);
                 else resolve(result[0]);
-            })
-        })
-    }
-}
+            });
+        });
+    },
+};
 
 module.exports = achieveModel;
